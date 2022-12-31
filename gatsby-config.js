cfca2c6df1dda9e2074108d089214ff9a9dc2848
Dima Veremchuk <verd@hey.com>
2022-12-31 07:09:42 +0000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Dima Veremchuk`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
