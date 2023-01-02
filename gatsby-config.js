871178ecd841520b5e6c947d9d79fa050c8d08cc
Dima Veremchuk <verd@hey.com>
2023-01-02 18:43:06 +0000

    favicon fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Dima Veremchuk`,
     description: `Dima Veremchuk—product designer. Personal website.`,
-    siteUrl: `https://www.dimaveremchuk.com`,
+    siteUrl: `https://dimaveremchuk.com`,
     image: `/icon.png`,
   },
   plugins: [
@@ -15,6 +15,19 @@ module.exports = {
           include: /svg/
         }
       }
-    }
+    },
+    // {
+    //   resolve: 'gatsby-plugin-manifest',
+    //   options: {
+    //     name: "Dima Veremchuk",
+    //     short_name: "Dima Veremchuk",
+    //     start_url: "/",
+    //     background_color: "#ffffff",
+    //     theme_color: "#fff",
+    //     display: "standalone",
+    //     icon: "src/images/icon.png",
+    //     crossOrigin: `use-credentials`,
+    //   },
+    // },
   ],
 }
